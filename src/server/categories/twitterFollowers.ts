0be6b150c83fbacf4b84dc4e2cf9f674b39ab2d9
benleong0 <benleong@myurbanjungle.com>
2022-09-03 21:11:47 +0100

    new category

---
+++ src/server/categories/twitterFollowers.ts
@@ -0,0 +1,17 @@
+const EXAMPLE_OPTIONS = {
+    "category": "twitterFollowers",
+    "pretext": "has",
+    "posttext": "twitter followers",
+    "options": [
+        {
+            "id": "justinbeiber",
+            "title": "@justinbeiber",
+            "value": 144148254,
+            "img": "https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg"
+        },
+    ]
+}
+
+export default function getOptions() {
+    return EXAMPLE_OPTIONS;
+}
